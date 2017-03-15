8f441d6d05d8601b4987231974fa3ffc25f67dcd
cerico <garethrobertlee@gmail.com>
2017-03-15 20:14:49 +0000

    update to include reducers

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
 import { View , Text } from 'react-native';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
 const App = () => {
 	return (
+		<Provider store={createStore(reducers)}>
 		<View>
 		    <Text>Clone me!</Text>
         </View>
+        </Provider>
 	);
 };
 
